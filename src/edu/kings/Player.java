0d9f5c74495be12e8e71494938a3c2723b617a41
vincentciavarella <vincentciavarella@MC41302.kings.edu>
2026-02-25 15:30:28 -0500

    Player Class and design doc

---
+++ src/edu/kings/Player.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package edu.kings;
+
+/**
+ * 
+ */
+public class Player 
+{
+	String room = "";
+	String currentRoom = "";
+	
+	//constructor and takes what room
+	public Player(String room)
+	{
+		room = "bedroom";
+	}
+	
+	public String getRoom()
+	{
+		return room;
+	}
+	
+	public void setRoom()
+	{
+		room = currentRoom;
+	}
+
+}
